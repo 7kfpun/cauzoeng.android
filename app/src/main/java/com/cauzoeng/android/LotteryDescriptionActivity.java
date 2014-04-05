3d180e01bf69de8fbc87361ef226cb658da986d0
kf <7kfpun@gmail.com>
2014-04-05 12:48:45 +0800

    remove import

--- app/src/main/java/com/cauzoeng/android/LotteryDescriptionActivity.java
+++ app/src/main/java/com/cauzoeng/android/LotteryDescriptionActivity.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -68,7 +67,7 @@ public class LotteryDescriptionActivity extends FragmentActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings_0) {
             return true;
         }
         return super.onOptionsItemSelected(item);
