c0e3f5d452355d1999ea137e8c283b097c26e804
kf <7kfpun@gmail.com>
2014-05-09 18:31:33 +0800

    add map clustering

--- app/src/main/java/com/cauzoeng/android/DescriptionActivity.java
+++ app/src/main/java/com/cauzoeng/android/DescriptionActivity.java
@@ -1,8 +1,8 @@
 package com.cauzoeng.android;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
--- app/src/main/java/com/cauzoeng/android/MapActivity.java
+++ app/src/main/java/com/cauzoeng/android/MapActivity.java
@@ -1,37 +1,78 @@
 package com.cauzoeng.android;
 
-import android.app.Activity;
+
+import com.cauzoeng.android.model.MyItem;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import android.support.v4.app.FragmentActivity;
+import com.google.maps.android.clustering.ClusterManager;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.json.JSONException;
+
 
 public class MapActivity extends FragmentActivity {
-    static final LatLng TutorialsPoint = new LatLng(21 , 57);
-    private GoogleMap googleMap;
+    private GoogleMap mMap;
+
+    protected int getLayoutId() {
+        return R.layout.activity_map;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        try {
-            if (googleMap == null) {
-                googleMap = ((MapFragment) getFragmentManager().
-                        findFragmentById(R.id.map)).getMap();
-            }
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        setContentView(getLayoutId());
+        setUpMapIfNeeded();
+    }
 
-            Marker TP = googleMap.addMarker(new MarkerOptions().
-                    position(TutorialsPoint).title("TutorialsPoint"));
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void setUpMapIfNeeded() {
+        if (mMap != null) {
+            return;
         }
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (mMap != null) {
+            startDemo();
+        }
+    }
 
+    private ClusterManager<MyItem> mClusterManager;
+
+    protected void startDemo() {
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+
+        mClusterManager = new ClusterManager<MyItem>(this, getMap());
+        getMap().setOnCameraChangeListener(mClusterManager);
+
+        try {
+            readItems();
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void readItems() throws JSONException {
+        InputStream inputStream = getResources().openRawResource(R.raw.radar_search);
+        List<MyItem> items = new MyItemReader().read(inputStream);
+        mClusterManager.addItems(items);
+    }
+
+    protected GoogleMap getMap() {
+        setUpMapIfNeeded();
+        return mMap;
     }
 
 }
